b5ae68381d2bce0b209766a127b61275078dbd50
Claudia Mihaela Diosan <mcd1591@gmail.com>
2019-07-31 20:45:38 +0200

    removed birthyear from donor

--- app/models/donor.model.js
+++ app/models/donor.model.js
@@ -44,7 +44,6 @@ function validateDonor(donor){
     completeName: Joi.string().min(2).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().regex(/^[a-zA-Z0-9]{6,16}$/).min(6).required(),
-    birthyear: Joi.number().integer().min(1900).max(2003).positive().required(),
     donationType: Joi.string().valid("Food","Clothes","Work","Lodging","Work","Hygiene products").required().description("Accepted values:Food,Clothes,Work,Lodging,Hygiene products"),
     city: Joi.string().min(3).max(20).valid("Lleida","Barcelona","New York").required().description("Accepted values:Lleida,Barcelona,New York"),
     location: Joi.array().ordered([Joi.number().min(-180).max(180).required(),Joi.number().min(-90).max(90).required()]).description("Please use this format [ longitude, latitude]"),
